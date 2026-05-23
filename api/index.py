2d0d089fc94241172471a3938bd9d2c61afd0808
Aniket vijay shinde <aniketvijayshinde@Anikets-MacBook-Air.local>
2026-05-23 09:56:45 +0530

    Fix Vercel Flask deployment

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
+
 import sys
 import os
+from flask import Flask
 
+# allow import from root folder
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app
 
-# REQUIRED for Vercel
-def handler(event, context):
+# Vercel needs THIS variable
+app = app
+
+# handler for Vercel
+def handler(request):
     return app
 
